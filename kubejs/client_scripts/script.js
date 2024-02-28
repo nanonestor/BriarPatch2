880460b8a1ef2a4022e70cf649c07a0ee1156a62
nanonestor <68798571+nanonestor@users.noreply.github.com>
2024-02-27 20:27:42 -0800

    Add files via upload

---
+++ kubejs/client_scripts/script.js
@@ -0,0 +1,13 @@
+// priority: 0
+
+console.info('KubeJS loading client_scripts')
+
+//onEvent('jei.remove.categories', event => {
+//	event.remove(['create:automatic_shaped', 'create:automatic_shapeless', 'create:automatic_packing'])
+//  })
+
+
+// onEvent('jei.hide.items', event => {
+	// Hide items in JEI here
+	// event.hide('minecraft:cobblestone')
+// })
