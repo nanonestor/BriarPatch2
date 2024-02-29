877468e0de4ecc2eacf955f2df380218698a0064
Sam Terrazas <thechaoselement@gmail.com>
2024-02-29 16:33:18 -0700

    recipe no longer needed

--- kubejs/server_scripts/ie_fixes.js
+++ kubejs/server_scripts/ie_fixes.js
@@ -4,15 +4,6 @@ ServerEvents.recipes((event) => {
   event
     .shapeless("material_elements:coke_block", ["immersiveengineering:coke"])
     .id("kubejs:material_elements_coke_block");
-
-  event
-    .shaped("immersiveengineering:furnace_heater", ["ICI", "CWC", "IRI"], {
-      I: "#forge:ingots/iron",
-      C: "#forge:ingots/copper",
-      W: "immersiveengineering:wirecoil_copper",
-      R: "#forge:dusts/redstone",
-    })
-    .id("kubejs:immersiveengineering_furnace_heater");
 });
 
 console.log("Loaded IE Fixes");
