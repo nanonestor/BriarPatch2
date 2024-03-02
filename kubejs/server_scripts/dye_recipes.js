09d21b82ca42df29a46372fa3a398e731ea19baf
Sam Terrazas <thechaoselement@gmail.com>
2024-03-02 13:42:37 -0700

    Should have been with the previous branch

--- kubejs/server_scripts/dye_recipes.js
+++ kubejs/server_scripts/dye_recipes.js
@@ -1,26 +1,3 @@
-console.log("KubeJS loading dye recipes...");
-
-// COLORS declared in kubejs/server_scripts/script.js
-
-let COLORS = [
-  "black",
-  "blue",
-  "brown",
-  "cyan",
-  "gray",
-  "green",
-  "light_blue",
-  "light_gray",
-  "lime",
-  "magenta",
-  "orange",
-  "pink",
-  "purple",
-  "red",
-  "white",
-  "yellow",
-];
-
 const dyeRecipe = (event, dye_item, amount) => {
   event.shaped(Item.of(dye_item, amount), ["A A", " B ", "A A"], {
     A: "thermal:phytogro",
@@ -30,11 +7,9 @@ const dyeRecipe = (event, dye_item, amount) => {
 
 // Add recipes here
 ServerEvents.recipes((event) => {
-  COLORS.forEach((color) => {
+  global.COLORS.forEach((color) => {
     dyeRecipe(event, `minecraft:${color}_dye`, 16);
   });
 
   event.smelting("minecraft:green_dye", "minecraft:grass", 1, 400);
 });
-
-console.log("KubeJS loaded dye recipes.");
