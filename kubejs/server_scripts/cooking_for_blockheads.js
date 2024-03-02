9101e2313f21bd455bc5f28c48acb0c166448a87
Sam Terrazas <thechaoselement@gmail.com>
2024-03-02 13:37:08 -0700

    Added constants
    Added constants and fixed up some of the mek things

--- kubejs/server_scripts/cooking_for_blockheads.js
+++ kubejs/server_scripts/cooking_for_blockheads.js
@@ -1,28 +1,7 @@
 console.log("KubeJS loading cooking_for_blockheads.js...");
 
-// COLORS declared in kubejs/server_scripts/script.js
-
-let COLORS = [
-  "black",
-  "blue",
-  "brown",
-  "cyan",
-  "gray",
-  "green",
-  "light_blue",
-  "light_gray",
-  "lime",
-  "magenta",
-  "orange",
-  "pink",
-  "purple",
-  "red",
-  "white",
-  "yellow",
-];
-
 ServerEvents.recipes((event) => {
-  COLORS.forEach((color) => {
+  global.COLORS.forEach((color) => {
     event.remove({ id: `cookingforblockheads:dye_${color}_oven` });
 
     const colorTag = color !== "white" ? `${color}_` : "";
---
+++ kubejs/startup_scripts/constants.js
@@ -0,0 +1,29 @@
+// priority: 10
+// Load before all other scripts
+
+global.COLORS = [
+  "black",
+  "blue",
+  "brown",
+  "cyan",
+  "gray",
+  "green",
+  "light_blue",
+  "light_gray",
+  "lime",
+  "magenta",
+  "orange",
+  "pink",
+  "purple",
+  "red",
+  "white",
+  "yellow",
+];
+
+global.MEK_TIERS = ["basic", "advanced", "elite", "ultimate"];
+global.MEK_PIPES = [
+  "pressurized_tube",
+  "mechanical_pipe",
+  "logistical_transporter",
+  "universal_cable",
+];
