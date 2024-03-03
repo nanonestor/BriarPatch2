0b66211f87075ad4089ffd837d76fb54ad860a80
nanonestor <68798571+nanonestor@users.noreply.github.com>
2024-03-02 22:00:56 -0800

    Delete kubejs/server_scripts/vanilla_fixes.js

--- kubejs/server_scripts/vanilla_fixes.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.recipes((event) => {
-  event.shapeless("minecraft:oak_sign", ["#minecraft:signs"]);
-  event.shapeless("minecraft:oak_hanging_sign", ["#minecraft:hanging_signs"]);
-});
